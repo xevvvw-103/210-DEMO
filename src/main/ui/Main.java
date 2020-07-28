4f076fc619b2168c6b5b92cd73c6a8069c607838
xevvvw <xevvvw@students.cs.ubc.ca>
2020-07-27 17:00:07 -0700

    add a ui interface

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,18 @@
 package ui;
 
+import model.Recipe;
+
 public class Main {
     public static void main(String[] args) {
+
+        // MODIFIES: this
+        // EFFECTS: run user input
+        private void runConsole() {
+            // press f for feed puppy
+            // press w for walk puppy
+            // stub
+        }
+
 
     }
 }
