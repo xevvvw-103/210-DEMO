5ae354cfc067114ae28c398238e9c2111bc54bec
xevvvw <xevvvw@students.cs.ubc.ca>
2020-07-27 17:08:54 -0700

    fix ui codes

---
+++ src/main/ui/ConsoleApp.java
@@ -0,0 +1,18 @@
+package ui;
+
+// user interface application
+public class ConsoleApp {
+
+    //EFFECTS: run the console application
+    public ConsoleApp() {
+        runConsole();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: process the user input
+    private void runConsole() {
+        // press r for design your recipe for puppy.
+        // press f for feed puppy with your designed recipe.
+        // press w for walk puppy which you could randomly get dishes as a reward.
+    }
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,15 +4,6 @@ import model.Recipe;
 
 public class Main {
     public static void main(String[] args) {
-
-        // MODIFIES: this
-        // EFFECTS: run user input
-        private void runConsole() {
-            // press f for feed puppy
-            // press w for walk puppy
-            // stub
-        }
-
-
+        new consoleApp();
     }
 }
